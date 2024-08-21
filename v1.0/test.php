986c8d2c1c654a5281c2d72d6e81e8e6cd8d3228
oleg <oleg@artisansoft.fi>
2024-08-21 17:05:13 +0300

    Initial commit

---
+++ v1.0/test.php
@@ -0,0 +1,35 @@
+<?php
+
+use EDI\Analyser;
+use EDI\Interpreter;
+use EDI\Mapping\MappingProvider;
+use EDI\Parser;
+
+require 'vendor/autoload.php';
+
+$data = parseString('editest.txt');
+
+file_put_contents('editest.json', json_encode($data, JSON_INVALID_UTF8_SUBSTITUTE | JSON_PRETTY_PRINT));
+
+print_r($data);
+
+function parseString(string $filename): array
+{
+    $parser = new Parser();
+    $parser->load($filename);
+    $parsed = $parser->get();
+
+    $analyser = new Analyser();
+    $mapping = new MappingProvider('D96A');
+    $segs = $analyser->loadSegmentsXml($mapping->getSegments());
+    $svc = $analyser->loadSegmentsXml($mapping->getServiceSegments());
+
+    $interpreter = new Interpreter($mapping->getMessage('ORDERS'), $segs, $svc);
+    $groups = $interpreter->prepare($parsed);
+
+
+    return [
+        'errors' => $interpreter->getErrors(),
+        'groups' =>$groups,
+    ];
+}
